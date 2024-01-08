819cad695209f312ae966fdb6c142425233ca494
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-08 19:13:34 +0530

    deleting the mid element of the ll

--- DSA_Basics/src/LinkedList_Int/LinkedList.java
+++ DSA_Basics/src/LinkedList_Int/LinkedList.java
@@ -524,6 +524,26 @@ public class LinkedList {
 			            headFirst.next=null;
 			        }
 		}
+		
+		//deleting the mid element of the ll
+		 public Node deleteMiddle(Node head) {
+		        if(head==null || head.next==null){
+		            return null;
+		        }
+		        
+		        // ListNode slow=head;
+		        Node fast=head;
+		        Node dummy=new Node(0);
+		        dummy.next=head;
+		        Node slow=dummy;
+		        while(fast!=null && fast.next!=null){
+		            slow=slow.next;
+		            fast=fast.next.next;
+		        }
+		        slow.next=slow.next.next;
+		        return head;
+		        
+		    }
 
 		public static void main(String[] args) {
 //		LinkedList list=new LinkedList();
