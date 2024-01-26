170788d719a6c0d9a818614cf6f29e2847b8da96
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-26 23:52:01 +0530

    Queue using stack

---
+++ DSA_Basics/src/STACK/QueueUsing_Stack.java
@@ -0,0 +1,14 @@
+package STACK;
+
+public class QueueUsing_Stack {
+	
+	public QueueUsing_Stack() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
