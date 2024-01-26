a9b932cea8a4669a762daee6d318a37ddf10971c
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-26 23:56:13 +0530

    queue implementation using 2 stacks

--- DSA_Basics/src/STACK/QueueUsing_Stack.java
+++ DSA_Basics/src/STACK/QueueUsing_Stack.java
@@ -1,11 +1,46 @@
 package STACK;
 
+import java.util.Stack;
+
 public class QueueUsing_Stack {
-	
+	Stack<Integer> first;
+	Stack<Integer> second;
 	public QueueUsing_Stack() {
-		
+		first=new Stack<>();
+		second=new Stack<>();
 	}
 
+	public void add(int x) {
+		first.push(x);
+	}
+	
+	public int remove() {
+		while(!first.isEmpty()) {
+			second.push(first.pop());
+		}
+		int removed=second.pop();
+		while(!second.isEmpty()) {
+			first.push(second.pop());
+		}
+		return removed;
+	}
+	
+	public int peek() {
+		while(!first.isEmpty()) {
+			second.push(first.pop());
+		}
+		int peeked=second.peek();
+		while(!second.isEmpty()) {
+			first.push(second.pop());
+		}
+		return peeked;
+	}
+	
+	public boolean empty() {
+		return first.isEmpty();
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
