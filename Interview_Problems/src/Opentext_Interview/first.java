e7312ea12baadcdab0ab5e10190bfa6a1424cce8
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-02-01 22:21:33 +0530

    Opentext experience

---
+++ Interview_Problems/src/Opentext_Interview/first.java
@@ -0,0 +1,24 @@
+package Opentext_Interview;
+
+public class first {
+	private static int method() {
+		System.out.println("a");
+		return 1;
+	}
+	private static String method() {
+		System.out.println(1);
+		return "a";
+	}
+
+	public static void main(String[] args) {
+	int a=method();
+	String ans=method();
+
+	}
+
+}
+
+
+//  o/p:
+//compilation problem: 
+//	Type mismatch: cannot convert from int to String
